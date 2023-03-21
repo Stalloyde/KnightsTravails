fae6e164699fb87501d019e5d02c2833fa380550
Stalloyde <stalloyde93@gmail.com>
2023-03-21 14:22:53 +0800

    WIP: To add grandchildren nodes

--- src/knightsmove.js
+++ src/knightsmove.js
@@ -1,13 +1,12 @@
 import { add } from 'mathjs';
 
-function nodeFactory(value, descendants = []) {
-  value = value;
-  return { value, descendants };
+function nodeFactory(value, parentNode, children = []) {
+  return { value, parentNode, children };
 }
 
 function knightsMove(root, targetPosition) {
-  const possibleMovesArray = [];
-  function getPossibleMoves() {
+  function getPossibleMoves(root) {
+    const possibleMovesArray = [];
     const moveVectors = [
       [2, 1],
       [2, -1],
@@ -20,6 +19,9 @@ function knightsMove(root, targetPosition) {
     ];
 
     moveVectors.forEach((item) => {
+      if (!root) {
+        return;
+      }
       const moveOption = add(root, item);
       if (
         moveOption[0] > 0 &&
@@ -30,22 +32,39 @@ function knightsMove(root, targetPosition) {
         possibleMovesArray.push(moveOption);
       }
     });
+    return possibleMovesArray;
   }
 
-  function buildTree(root, queue = []) {
-    if (root[0] === targetPosition[0] && root[1] === targetPosition[1]) {
-      return root;
-    }
+  function buildGraph(
+    root,
+    queue = [],
+    visitedNode = [root],
+    parentNode = null
+  ) {
+    let rootNode = nodeFactory(root, root);
 
-    const rootNode = nodeFactory(root);
-    getPossibleMoves();
-    console.log(possibleMovesArray);
-    possibleMovesArray.forEach((item) => {
-      rootNode.descendants.push(item);
+    const array = getPossibleMoves(root); //3,2
+    array.forEach((item) => {
+      if (!visitedNode.includes(item) && !queue.includes(item)) {
+        rootNode.children.push(nodeFactory(item, rootNode)); //3,2's children = 5,3... 5,1... 4,4
+        visitedNode.push(item);
+        queue.push(item);
+      }
     });
-    console.log(rootNode);
-  }
 
-  return buildTree(root);
+    rootNode.children.forEach((item) => {
+      if (item.value.join() === targetPosition.join()) {
+        queue.length = 0;
+      }
+
+      while (queue.length > 0) {
+        const newRoot = queue[0]; //3,.2
+        queue.shift(); //2,3
+        buildGraph(newRoot, queue, visitedNode, parentNode); //3,2, 2,3
+      }
+    });
+    return rootNode;
+  }
+  return buildGraph(root);
 }
-knightsMove([1, 1], [5, 4]);
+console.log(knightsMove([1, 1], [3, 7]));
