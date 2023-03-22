857e402075c5d56d2af6ecefa5100cc3d09423ed
Stalloyde <stalloyde93@gmail.com>
2023-03-22 22:29:47 +0800

    Build full graph - WIP: Traverse and retrieve shortest path

--- src/knightsmove.js
+++ src/knightsmove.js
@@ -4,66 +4,65 @@ function nodeFactory(value, parentNode, children = []) {
   return { value, parentNode, children };
 }
 
+function getPossibleMoves(root) {
+  const possibleMovesArray = [];
+  const moveVectors = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [-1, 2],
+    [-1, -2],
+    [1, -2],
+  ];
+
+  moveVectors.forEach((item) => {
+    if (!root) {
+      return;
+    }
+    const moveOption = add(root, item);
+    if (
+      moveOption[0] > 0 &&
+      moveOption[1] > 0 &&
+      moveOption[0] < 9 &&
+      moveOption[1] < 9
+    ) {
+      possibleMovesArray.push(moveOption);
+    }
+  });
+  return possibleMovesArray;
+}
+
 function knightsMove(root, targetPosition) {
-  function getPossibleMoves(root) {
-    const possibleMovesArray = [];
-    const moveVectors = [
-      [2, 1],
-      [2, -1],
-      [-2, 1],
-      [-2, -1],
-      [1, 2],
-      [-1, 2],
-      [-1, -2],
-      [1, -2],
-    ];
-
-    moveVectors.forEach((item) => {
-      if (!root) {
-        return;
-      }
-      const moveOption = add(root, item);
-      if (
-        moveOption[0] > 0 &&
-        moveOption[1] > 0 &&
-        moveOption[0] < 9 &&
-        moveOption[1] < 9
-      ) {
-        possibleMovesArray.push(moveOption);
-      }
-    });
-    return possibleMovesArray;
-  }
-
   function buildGraph(
-    root,
+    currentNode,
     queue = [],
-    visitedNode = [root],
+    visitedNode = [currentNode],
     parentNode = null
   ) {
-    let rootNode = nodeFactory(root);
+    let rootNode = nodeFactory(currentNode);
 
-    const array = getPossibleMoves(root);
-    array.forEach((item) => {
-      if (!visitedNode.includes(item) && !queue.includes(item)) {
-        rootNode.children.push(nodeFactory(item, rootNode));
-        visitedNode.push(item);
-        queue.push(item);
+    const movesArray = getPossibleMoves(currentNode);
+    movesArray.forEach((moves) => {
+      if (!visitedNode.includes(moves) && !queue.includes(moves)) {
+        rootNode.children.push(nodeFactory(moves, rootNode));
+        visitedNode.push(moves);
+        queue.push(moves);
       }
     });
 
-    rootNode.children.forEach((item) => {
-      if (item.value.join() === targetPosition.join()) {
+    rootNode.children.forEach((childNode) => {
+      if (childNode.value.join() === targetPosition.join()) {
         queue.length = 0;
+        return targetPosition;
       }
 
-      while (queue.length > 0) {
-        const newRoot = queue[0];
-        queue.shift();
-        buildGraph(newRoot, queue, visitedNode, parentNode);
-      }
+      const newNode = queue[0];
+      queue.shift();
+      childNode.children = buildGraph(newNode, queue, visitedNode, parentNode);
     });
-    return rootNode.children;
+    return rootNode;
   }
   return buildGraph(root);
 }
