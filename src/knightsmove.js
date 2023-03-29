c47394a69f4c32ac37757841a14c4336ed620f1b
Stalloyde <stalloyde93@gmail.com>
2023-03-29 09:37:19 +0800

    Rebuilt tree with iterative approach

--- src/knightsmove.js
+++ src/knightsmove.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-syntax */
-import { toSafeInteger } from 'lodash';
 import { add } from 'mathjs';
 
 function getPossibleMoves(root) {
@@ -32,81 +31,33 @@ function getPossibleMoves(root) {
   return possibleMovesArray;
 }
 
-function nodeFactory(value, parentNode) {
+function nodeFactory(value, parentNode = null) {
   return { value, parentNode };
 }
 
 function knightsMove(root, targetPosition) {
-  function buildTree(
-    currentNode,
-    queue = [],
-    visitedNode = [currentNode],
-    childNodeArray = []
-  ) {
-    let rootNode = root;
+  function buildTree(root) {
+    const rootNode = nodeFactory(root);
+    const queue = [rootNode];
 
-    if (queue.length === 0) {
-      rootNode = nodeFactory(root, root);
-    } else {
-      rootNode = currentNode;
-    }
-
-    const movesArray = getPossibleMoves(currentNode);
-    movesArray.forEach((moves) => {
-      const serialisedVisitedNode = JSON.stringify(visitedNode);
-      const serialisedMove = JSON.stringify(moves);
-      if (serialisedVisitedNode.indexOf(serialisedMove) === -1) {
-        childNodeArray.push(nodeFactory(moves, rootNode)); //Pass grandparentNode to child node
-        visitedNode.push(moves);
-        queue.push(moves);
-      }
-    });
-
-    rootNode.children = childNodeArray;
-    rootNode.children.forEach((child) => {
-      if (child.value.join() === targetPosition.join()) {
+    while (queue.length > 0) {
+      const childNodeArray = [];
+      const currentNode = queue.shift();
+      const movesArray = getPossibleMoves(currentNode.value);
+      movesArray.forEach((moves) => {
+        const childNode = nodeFactory(moves, currentNode);
+        childNodeArray.push(childNode);
+        queue.push(childNode);
+      });
+      currentNode.children = childNodeArray;
+      if (currentNode.value.join() === targetPosition.join()) {
         queue.length = 0;
+        break;
       }
-      const newNode = queue[0];
-      queue.shift();
-      child = buildTree(newNode, queue, visitedNode, []); //Pass rootNode as grandparentNode to child
-    });
+    }
     return rootNode;
   }
-
   const rootNode = buildTree(root);
-
-  function findShortestPath(root, queue = [root], path = [root.value]) {
-    if (!root || !queue.length) return path;
-
-    if (root.value === rootNode.value) {
-      root.children.forEach((child) => queue.push(child));
-    }
-
-    if (root.value.children) {
-      root.value.children.forEach((child) => {
-        const node = {
-          value: [child.value[0], child.value[1]],
-          children: child.value.children,
-        };
-        queue.push(node);
-        if (node.children) {
-          for (const child of node.children) {
-            if (child.value.join() === targetPosition.join()) {
-              queue.length = 0;
-              path.push(child.value);
-            }
-          }
-        }
-      });
-    }
-
-    const newRoot = queue[0];
-    queue.shift();
-    return findShortestPath(newRoot, queue, path);
-  }
-
-  return findShortestPath(rootNode);
+  return rootNode;
 }
-
 console.log(knightsMove([1, 1], [5, 4]));
